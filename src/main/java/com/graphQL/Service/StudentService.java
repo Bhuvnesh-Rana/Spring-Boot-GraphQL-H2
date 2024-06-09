1953198a00f81941eeab2856eebb8cd8c5cededd
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-06-09 12:46:52 +0530

    Service mthds declared.

--- src/main/java/com/graphQL/Service/StudentService.java
+++ src/main/java/com/graphQL/Service/StudentService.java
@@ -1,7 +1,13 @@
 package com.graphQL.Service;
 
+import java.util.List;
+
 import com.graphQL.DAO.StudentDAO;
 
 public interface StudentService{
     String addStudent(StudentDAO studentDAO);
+    String updateStudent(StudentDAO studentDAO, int id);
+    List<StudentDAO> getAllStudents();
+    StudentDAO getStudentById(int id);
+    void deleteStudentById(int id);
 }
