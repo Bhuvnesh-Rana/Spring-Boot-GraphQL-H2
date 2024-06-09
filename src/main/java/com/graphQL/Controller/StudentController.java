10ea0d141560f13769ef6017183ca5890e349b86
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-06-09 15:50:52 +0530

    Controller using REST.

--- src/main/java/com/graphQL/Controller/StudentController.java
+++ src/main/java/com/graphQL/Controller/StudentController.java
@@ -1,7 +1,13 @@
 package com.graphQL.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +19,28 @@ public class StudentController {
     
     @Autowired
     private StudentService studentService;
-    @PostMapping("/add")
-    public String add(@RequestBody StudentDAO studentDAO){
+    @PostMapping("/stu")
+    public String addStudent(@RequestBody StudentDAO studentDAO){
         return studentService.addStudent(studentDAO);
+    }
+
+    @PutMapping("/stu/{id}")
+    public String updateStudent(@RequestBody StudentDAO studentDAO, int id){
+        return studentService.updateStudent(studentDAO, id);
+    }
+
+    @GetMapping("/stu")
+    public List<StudentDAO> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/stu/{id}")
+    public StudentDAO getStudenById(@PathVariable int id){
+        return studentService.getStudentById(id);
+    }
+
+    @DeleteMapping("/stu/{id}")
+    public void delete(@PathVariable int id){
+        studentService.deleteStudentById(id);
     }
 }
